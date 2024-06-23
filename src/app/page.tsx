94e98e3484c668ffec56b3f4aec9d088f9302935
ZakariyaShuja <shuja2020@gmail.com>
2024-06-23 22:46:03 +0500

    Mini

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,19 @@
 import Image from "next/image";
 import { CalendarForm } from "./componet/Calendar";
-import FavNumber from "./componet/Local";
-import FavNumber1 from "./componet/Local2";
-import FavNumber2 from "./componet/Local3";
+
 
 export default function Home() {
   return (
     <div>
-    <div className="bg-slate-800">
-      <div className="flex justify-center items-center pt-80 pb-80">
-        <CalendarForm />
+      <div className="bg-slate-800">
+        <div className="flex justify-center items-center pt-80 pb-80">
+          <CalendarForm />
+
+        </div>
 
       </div>
-  
-    </div>
-    <div className="flex justify-center items-center pt-44 bg-black pb-10">
-<FavNumber2/>
-</div>
-    
-<div className="flex justify-center items-center  bg-black pb-10">
-<FavNumber/>
-</div>
 
-<div className="flex justify-center items-center  bg-black pb-10">
-<FavNumber1/>
-</div>
-<div className="flex justify-center items-center text-lg pt-3">    <p>All rights reserved by Zakariya Bin Shuja &copy; 2024</p></div>
+      <div className="flex justify-center items-center text-lg pt-3">    <p>All rights reserved by Zakariya Bin Shuja &copy; 2024</p></div>
     </div>
   );
 }
